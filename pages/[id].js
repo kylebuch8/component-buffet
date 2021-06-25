e80703bce96882003ca91e91cfd9ff9c3e167ca7
Kyle Buchanan <kylebuch8@gmail.com>
2021-06-25 09:57:24 -0400

    adding some initial data

--- pages/[id].js
+++ pages/[id].js
@@ -1,20 +1,17 @@
 import Head from 'next/head'
 
-export default function Home({ collections }) {
+export default function Collection(params) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Collection</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to the Component Buffet
+          Welcome to the { params.id } page
         </h1>
-        {collections.map((collection, index) => (
-          <h2 key={ index }>{ collection }</h2>
-        ))}
       </main>
 
       <footer>
@@ -170,17 +167,35 @@ export default function Home({ collections }) {
   )
 }
 
-export async function getStaticProps() {
+export async function getStaticPaths() {
+  const paths = [
+    {
+      params: {
+        id: "patternfly-elements"
+      }
+    },
+    {
+      params: {
+        id: "chapeaux"
+      }
+    },
+    {
+      params: {
+        id: "one-platform"
+      }
+    }
+  ];
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
   // const res = await fetch(`https://.../posts/${params.id}`)
   // const post = await res.json()
 
   // // Pass post data to the page via props
   // return { props: { post } }
   return {
-    props: {
-      collections: [
-        "pfe", "chapeaux"
-      ]
-    }
+    props: params
   };
 }
